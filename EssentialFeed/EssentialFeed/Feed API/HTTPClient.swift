1f2b72d992c2c1114bb15588d3c18cace12a049d
Akshay Ganesh <akshayiyerg@gmail.com>
2023-06-24 16:18:11 -0400

    Created RemoteFeedLoader and HTTPClient files

---
+++ EssentialFeed/EssentialFeed/Feed API/HTTPClient.swift
@@ -0,0 +1,18 @@
+//
+//  HTTPClient.swift
+//  EssentialFeed
+//
+//  Created by Akshay  on 2023-06-24.
+//
+
+import Foundation
+
+public enum HTTPClientResponse {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    
+    func get(from url: URL, completion: @escaping(HTTPClientResponse) -> Void)
+}
