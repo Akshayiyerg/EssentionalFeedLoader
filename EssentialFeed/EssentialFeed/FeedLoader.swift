bf9fe45acca093c7ebff6d8ea80a57350bcf7156
Akshay Ganesh <akshayiyerg@gmail.com>
2023-06-24 16:09:02 -0400

    Create FeedLoader File

---
+++ EssentialFeed/EssentialFeed/FeedLoader.swift
@@ -0,0 +1,17 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Akshay  on 2023-06-24.
+//
+
+import Foundation
+
+public enum LoadFeedResult {
+    case success([FeedItem])
+    case failure(Error)
+}
+
+public protocol FeedLoader {
+    func load(completion: @escaping(LoadFeedResult) -> Void)
+}
