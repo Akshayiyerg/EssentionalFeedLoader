ee8df610073afe41c304aada582ff499a83d08c9
Akshay Ganesh <akshayiyerg@gmail.com>
2023-07-29 13:38:39 -0400

    Does not delete cache on retrieval of empty case.

--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -90,6 +90,16 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         
         XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
     }
+    
+    func test_load_doesNotDeleteCacheOnEmptyCache() {
+        
+        let (sut, store) = makeSut()
+        
+        sut.load { _ in }
+        store.completeRetrievalWithEmptyCache()
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
 
     // MARK: - Helpers
     
