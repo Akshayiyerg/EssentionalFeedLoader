1bc7b0d05c0f2047f23e8a45a4d1d1ac3c1ece1a
Akshay Ganesh <akshayiyerg@gmail.com>
2023-08-11 17:52:35 -0400

    Deleting non empty previously inserted cache.

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -72,6 +72,11 @@ class CodableFeedStore {
     }
     
     func deleteCachedFeed(completion: @escaping FeedStore.DeletionCompletionTypealias) {
+        guard FileManager.default.fileExists(atPath: storeURL.path) else {
+            return completion(nil)
+        }
+        
+        try! FileManager.default.removeItem(at: storeURL)
         completion(nil)
     }
     
@@ -182,6 +187,21 @@ final class CodableFeedStoreTests: XCTestCase {
         
         sut.deleteCachedFeed { deletionError in
             XCTAssertNil(deletionError,"Expected empty cache deletion to succeed")
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+        
+        expect(sut, toRetrieve: .empty)
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+        
+        let sut = makeSUT()
+        insert((uniqueImageFeed().localItems, Date()), to: sut)
+        
+        let exp = expectation(description: "wait for cache deletion")
+        sut.deleteCachedFeed { deletionError in
+            XCTAssertNil(deletionError, "Expected non-empty cache deletion to be successful")
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1.0)
