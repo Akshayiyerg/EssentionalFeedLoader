fa15172ec6e87c6875b8ac0f3076c84c4433c505
Akshay Ganesh <akshayiyerg@gmail.com>
2023-09-09 16:36:19 -0400

    CoreDataFeedStore deleteFeed function deletes previously inserted cache.

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -87,6 +87,10 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     
     func test_delete_emptiesPreviouslyInsertedCache() {
         
+        let sut = makeSUT()
+        
+        assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
+        
     }
     
     func test_storeSideEffects_runSerially() {
