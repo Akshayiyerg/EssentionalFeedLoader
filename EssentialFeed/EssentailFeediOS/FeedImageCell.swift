d6bc11bbdba44873ff09aa99113755f58ac14656
Akshay Ganesh <akshayiyerganesh@gmail.com>
2023-12-01 17:20:25 -0500

    Rendered loaded feed on succesfull load completion.

---
+++ EssentialFeed/EssentailFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentailFeediOS
+//
+//  Created by Akshay  on 2023-11-21.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
