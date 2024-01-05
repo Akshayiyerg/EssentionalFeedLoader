20cce02b35dddb08236e99769b021fd1c6385246
Akshay Ganesh <akshayiyerganesh@gmail.com>
2024-01-04 20:57:02 -0500

    Extract "cancelImageDataLoader(from: URL)" from 'FeedImageDataLoader' protocol into a new 'FeedImageDataLoaderTask' protocol that represents a task that can be cancelled. This way we respect the Interface Segregation Principle and 'FeedImageDataLoader' implementation are not being forced to be stateful.

--- EssentialFeed/EssentailFeediOS/FeedViewController.swift
+++ EssentialFeed/EssentailFeediOS/FeedViewController.swift
@@ -5,13 +5,15 @@
 //  Created by Akshay  on 2023-11-21.
 //
 
-import Foundation
 import UIKit
 import EssentialFeed
 
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+
 public protocol FeedImageDataLoader {
-    func loadImageData(from url: URL)
-    func cancelImageDataLoader(from url: URL)
+    func loadImageData(from url: URL) -> FeedImageDataLoaderTask
 }
 
 final public class FeedViewController: UITableViewController {
@@ -19,6 +21,7 @@ final public class FeedViewController: UITableViewController {
     private var feedLoader: FeedLoader?
     private var imageLoader: FeedImageDataLoader?
     private var tableModel = [FeedImage]()
+    private var tasks = [IndexPath: FeedImageDataLoaderTask]()
     
     public convenience init(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) {
         self.init()
@@ -56,12 +59,12 @@ final public class FeedViewController: UITableViewController {
         cell.locationContainer.isHidden = (cellModel.location == nil)
         cell.locationLabel.text = cellModel.location
         cell.descriptionLabel.text = cellModel.description
-        imageLoader?.loadImageData(from: cellModel.url)
+        tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url)
         return cell
     }
     
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        let cellModel = tableModel[indexPath.row]
-        imageLoader?.cancelImageDataLoader(from: cellModel.url)
+        tasks[indexPath]?.cancel()
+        tasks[indexPath] = nil
     }
 }
